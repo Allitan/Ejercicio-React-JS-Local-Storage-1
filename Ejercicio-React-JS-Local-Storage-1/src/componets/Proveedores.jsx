94a8c02fb1476277a1b8a7dabaa793f3d5987de4
Allan <117397343+Allitan@users.noreply.github.com>
2025-06-17 23:35:01 -0600

    update

--- Ejercicio-React-JS-Local-Storage-1/src/componets/Proveedores.jsx
+++ Ejercicio-React-JS-Local-Storage-1/src/componets/Proveedores.jsx
@@ -1,12 +1,37 @@
 import Campo from "./Campo"
+import useProveedor from "../hooks/useProveedor"
+import { useEffect } from "react"
 
 const Proveedores = () => {
+    const {
+        getProveedor,
+        proveedores,
+        setProveedores,
+        nombre,
+        setNombre,
+        apellido,
+        setApellido,
+        telefono,
+        setTelefono,
+        email,
+        setEmail,
+        openModal,
+        validar,
+        tituloModal,
+        deleteProveedor
+
+    } = useProveedor()
+
+    useEffect(() => { 
+        setProveedores(getProveedor())
+    }, [])
+
     return(
         <div className="container-fluid">
             <div className="row mt-3">
                 <div className="col-md-4 offset-md-4">
                     <div className="d-grid mx-auto">
-                        <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalProveedor"><i className="fa-solid fa-circle-plus" /> Añadir</button>
+                        <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalProveedor" onClick={() => openModal(1)}><i className="fa-solid fa-circle-plus" /> Añadir</button>
                     </div>    
                 </div>
             </div>
@@ -24,7 +49,24 @@ const Proveedores = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            
+                            {
+                                proveedores.map((proveedores, i) => {
+                                   return( 
+                                    <tr key={proveedores.id}>
+                                        <td>{i + 1}</td>
+                                        <td>{proveedores.nombre}</td>
+                                        <td>{proveedores.apellido}</td>
+                                        <td>{proveedores.telefono}</td>
+                                        <td>{proveedores.email}</td>
+                                        <td>
+                                            <button className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalProveedor" onClick={() => openModal(2, proveedores)}><i className="fa-solid fa-edit" /> Editar</button>
+
+                                            <button className="btn btn-danger" onClick={() => deleteProveedor(proveedores.id)}><i className="fa-solid fa-trash" /> Eliminar</button>
+                                        </td>
+                                    </tr>
+                                   ) 
+                                })
+                            }
                         </tbody>
                     </table>
                 </div>
@@ -34,16 +76,16 @@ const Proveedores = () => {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <label className="h5">Agregar Proveedor</label>
+                            <label className="h5"> {tituloModal} </label>
                         </div>
                         <div className="modal-body">
-                            <Campo id="nombre" iconName="fa-solid fa-user" inputType="text" placeHolder="Nombre" />
-                            <Campo id="apellido" iconName="fa-solid fa-user-tag" inputType="text" placeHolder="Apellido" />
-                            <Campo id="telefono" iconName="fa-solid fa-phone" inputType="number" placeHolder="Teléfono" />
-                            <Campo id="correo" iconName="fa-solid fa-envelope" inputType="email" placeHolder="Correo Electrónico" />
+                            <Campo id="nombre" iconName="fa-solid fa-user" inputType="text" placeHolder="Nombre" onChange={(e) => setNombre(e.target.value)} value={nombre} />
+                            <Campo id="apellido" iconName="fa-solid fa-user-tag" inputType="text" placeHolder="Apellido" onChange={(e) => setApellido(e.target.value)} value={apellido} />
+                            <Campo id="telefono" iconName="fa-solid fa-phone" inputType="number" placeHolder="Teléfono" onChange={(e) => setTelefono(e.target.value)} value={telefono} />
+                            <Campo id="correo" iconName="fa-solid fa-envelope" inputType="email" placeHolder="Correo Electrónico" onChange={(e) => setEmail(e.target.value)} value={email} />
                         </div>
                         <div className="modal-footer">
-                            <button className="btn btn-success"><i className="fa-solid fa-floppy-disk"/> Guardar</button>
+                            <button className="btn btn-success" onClick={() => validar()}><i className="fa-solid fa-floppy-disk"/> Guardar</button>
                             <button id="btnCerrarModal" className="btn btn-danger" data-bs-dismiss="modal"><i className="fa-solid fa-circle-xmark"/> Cerrar</button>
                         </div>
                     </div> 
--- Ejercicio-React-JS-Local-Storage-1/src/hooks/useProveedor.js
+++ Ejercicio-React-JS-Local-Storage-1/src/hooks/useProveedor.js
@@ -1,20 +1,133 @@
-import { useState } from "react";  
-import Swal from "sweetalert2";
-import { alertaSuccess, alertaError, alertaWarning} from "../alertas";
+import { useState } from "react" 
+import Swal from "sweetalert2"
+import { alertaSuccess, alertaError, alertaWarning} from "../alertas"
 
 const useProveedor = () => {
-    const [proveedores, setProveedores] = useState([]);
-    const [id, setId] = useState('');
-    const [nombre, setNombre] = useState('');
-    const [apellido, setApellido] = useState('');
-    const [telefono, setTelefono] = useState('');
-    const [email, setEmail] = useState('');
-    const [tituloModal, setTituloModal] = useState('');
-    const [operacion, setOperacion] = useState('');
+    const [proveedores, setProveedores] = useState([])
+    const [id, setId] = useState('')
+    const [nombre, setNombre] = useState('')
+    const [apellido, setApellido] = useState('')
+    const [telefono, setTelefono] = useState('')
+    const [email, setEmail] = useState('')
+    const [tituloModal, setTituloModal] = useState('')
+    const [operacion, setOperacion] = useState('')
 
     const getProveedor = () => {
-        const localStorageProveedores = localStorage.getItem('PROVEEDORES');
-        const parsedProveedores = localStorageProveedores ? JSON.parse(localStorageProveedores) : [];
+        const localStorageProveedores = localStorage.getItem('PROVEEDORES')
+        const parsedProveedores = localStorageProveedores ? JSON.parse(localStorageProveedores) : []
+        return Array.isArray(parsedProveedores) ? parsedProveedores : []
+    }
+
+    const enviarSolicitud = (metodo, parametros = {}) => {
+        const saveUpdateProveedor = [...proveedores]
+        let mensaje = ''
+
+        if (metodo === 'POST'){
+            saveUpdateProveedor.push({ ...parametros, id: Date.now()})
+            mensaje = 'Proveedor agregado correctamente'
+        } else if (metodo === 'PUT'){
+            const proveedorIndex = saveUpdateProveedor.findIndex(proveedor => proveedor.id === parametros.id)
+            
+            if(proveedorIndex !== -1){
+                saveUpdateProveedor[proveedorIndex] = {...parametros}
+                mensaje = 'Proveedor actualizado correctamente'
+            }
+        } else if (metodo === 'DELETE'){
+            const proveedorArr= saveUpdateProveedor.filter(proveedores => proveedores.id !== parametros.id)
+            localStorage.setItem('PROVEEDORES', JSON.stringify(proveedorArr))
+            alertaSuccess('Proveedor eliminado correctamente')
+            return
+        }
+
+        localStorage.setItem('PROVEEDORES', JSON.stringify(saveUpdateProveedor))
+        setProveedores(saveUpdateProveedor)
+        alertaSuccess(mensaje)
+        document.getElementById('btnCerrarModal').click()
+    }
+
+    const validar = () => {
+        let metodo = ''
+
+        if (nombre === ''){
+            alertaWarning('El campo nombre es obligatorio', 'nombre')
+        } else if (apellido === ''){
+            alertaWarning('El campo apellido es obligatorio', 'apellido')
+        } else if (telefono === ''){
+            alertaWarning('El campo teléfono es obligatorio', 'telefono')
+        } else if (email === ''){
+            alertaWarning('El campo correo electrónico es obligatorio', 'correo')
+        } else {
+            let payload = {
+                id: id || Date.now(),
+                nombre: nombre,
+                apellido: apellido,
+                telefono: telefono,
+                email: email
+            }
+
+            if (operacion === 1){
+                metodo = 'POST'
+            } else {
+                metodo = 'PUT'
+            }
+
+            enviarSolicitud(metodo, payload)
+        }
+    }
+
+    const deleteProveedor = (id) => {
+        Swal.fire({
+            title: '¿Estás seguro de eliminar el proveedor?',
+            text: "¡No podrás revertir esto!",
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Sí, eliminarlo',
+            cancelButtonText: 'cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                enviarSolicitud('DELETE', { id })
+            }
+        }).catch((error) => {
+            alertaError('Error al eliminar el proveedor', error)
+        })
+    }
+
+    const openModal = (valorOperacion, proveedor) => {
+        if (valorOperacion === 1) {
+            setTituloModal('Registrar Proveedor')
+            setId('')
+            setNombre('')
+            setApellido('')
+            setTelefono('')
+            setEmail('')
+            setOperacion(1)
+        } else if (valorOperacion === 2) {
+            setTituloModal('Editar Proveedor')
+            setId(proveedor.id)
+            setNombre(proveedor.nombre)
+            setApellido(proveedor.apellido)
+            setTelefono(proveedor.telefono)
+            setEmail(proveedor.email)
+            setOperacion(2)
+        }
+    }
+
+    return {
+        getProveedor,
+        proveedores,
+        setProveedores,
+        nombre,
+        setNombre,
+        apellido,
+        setApellido,
+        telefono,
+        setTelefono,
+        email,
+        setEmail,
+        openModal,
+        validar,
+        tituloModal,
+        deleteProveedor,
     }
 }
 
